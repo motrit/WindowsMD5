0f7fac89503b12f49648304e523bc3bd9b09db31
Iosig Koshich <motroniii@gmail.com>
2015-10-05 16:59:28 +0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -8,14 +8,20 @@ import sys
 import ctypes
 
 class disable_file_system_redirection:
-    _disable = ctypes.windll.kernel32.Wow64DisableWow64FsRedirection
-    _revert = ctypes.windll.kernel32.Wow64RevertWow64FsRedirection
-    def __enter__(self):
-        self.old_value = ctypes.c_long()
-        self.success = self._disable(ctypes.byref(self.old_value))
-    def __exit__(self, type, value, traceback):
-        if self.success:
-            self._revert(self.old_value)
+    try:
+        _disable = ctypes.windll.kernel32.Wow64DisableWow64FsRedirection
+        _revert = ctypes.windll.kernel32.Wow64RevertWow64FsRedirection
+        def __enter__(self):
+            self.old_value = ctypes.c_long()
+            self.success = self._disable(ctypes.byref(self.old_value))
+        def __exit__(self, type, value, traceback):
+            if self.success:
+                self._revert(self.old_value)
+    except AttributeError: #means that the script is running in non-Windows OS
+        def __enter__(self):
+            pass
+        def __exit__(self, type, value, traceback):
+            pass
 
 #byte size
 CHUNK_SIZE = 4096
@@ -208,7 +214,7 @@ if __name__ == "__main__":
     with disable_file_system_redirection():
         if(len(sys.argv) < 2):
             display_usage()
-    
+
         #sample.py --help
         if (sys.argv[1] == "--help"):
             print "Available commands:" \
